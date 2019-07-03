1b2906d53dc3aed2c8121aa8de2192544f61e0aa
syu <1838653805@qq.com>
2019-07-03 18:27:55 +0800

    merge

---
+++ app/meinfo/FHJ.js
@@ -0,0 +1,30 @@
+export function fetchWeiXinNewsListByPage(start, pageLimit) {
+    return dispatch => {
+        dispatch({
+            type: types.ACTION_WEIXIN_NEWS_PRE_FETCH,
+            isLoadingMore: true,
+        });
+        NetUtils.get(URL_WEI_XIN_JINGXUAN+'?key='+APP_KEY_WEI_XIN_JINGXUAN+'&num='+pageLimit+'&page='+start)
+        .then(function (result) {
+            if (result.code == 200) {
+                dispatch({
+                    type: types.ACTION_WEIXIN_NEWS_FETCH_OK,
+                    newsList: result.newslist,
+                    start: start,
+                    pageLimit: pageLimit,
+                    isLoadingMore: false,
+                });
+            } else {
+                dispatch({
+                type: types.ACTION_WEIXIN_NEWS_FETCH_ERROR,
+                isLoadingMore: false,
+            });
+            }
+        }, function () {
+            dispatch({
+                type: types.ACTION_WEIXIN_NEWS_FETCH_ERROR,
+                isLoadingMore: false,
+            });
+        })
+    };
+}
